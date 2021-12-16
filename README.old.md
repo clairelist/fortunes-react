578ddbfd10c0495c5123c6955a66fd3db45dec62
Claire List <claire.list2501@gmail.com>
2021-12-16 13:00:58 -0700

    code factored in react

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# fortunes-react
+Refactor of a fortune telling app I made for personal blog ages ago
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hello from inside of App !
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
